6db3b9a7cff1cf0ccbe21e0ff686ec0bed6a9e01
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-09-05 17:49:50 +0100

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
-    
+   <main className="min-h-dvh">
+    <section className="min-h-dvh bg-[url('/bg.jpg')] bg-no-repeat bg-center bg-cover">
+      <div className="min-h-dvh bg-black/60"></div>
+    </section>
    </main>
   );
 }
