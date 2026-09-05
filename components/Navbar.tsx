461bc86bc7821786a67f88ff938cb3fb00297300
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-09-05 17:39:30 +0100

    updates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,17 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { Theme } from "./Theme";
 import { IoIosMenu } from "react-icons/io";
 import { IoMdRestaurant } from "react-icons/io";
+import { useState } from "react";
 
 export default function Navbar () {
+
+    const [navOpen, setNavOpen] = useState(false)
+    // console.log(navOpen);
+    
     const navLinks: object[] = [
         {
             label: "Home",
@@ -26,7 +32,7 @@ export default function Navbar () {
     ]
     return (
         <main className="flex items-center justify-between lg:px-20 py-2 shadow-md relative">
-            <Link href={"/"} className="flex items-center">
+            <Link href={"/"} className="flex items-center z-50">
                 <Image
                     src={"/logo.jpg"}
                     alt="logo"
@@ -54,20 +60,24 @@ export default function Navbar () {
             </div>
 
             {/* mobile and tablet view */}
-            <button className="lg:hidden">
-                <IoIosMenu />
-                <IoMdRestaurant />
+            <button onClick={()=> setNavOpen(!navOpen)} className="lg:hidden z-50 text-3xl mr-2">
+                {
+                    navOpen ? <IoMdRestaurant /> : <IoIosMenu />
+                }               
             </button>
 
-            <blockquote className="lg:hidden">
-                <div className="bg-red-600 h-dvh absolute top-0 right-0 w-full">
+            <blockquote className={`lg:hidden absolute top-0 right-0 w-full h-dvh space-y-6 bg-white ${navOpen ? "block" : "hidden"}`}>
+                <div className="flex pt-20 flex-col gap-10 items-center">
                     {
                         navLinks.map((item, i)=> (
                             <Link href={item.url} className="text-lg">{item.label}</Link>
                         ))
                     }
                 </div>
-                
+                <div className="flex flex-col items-center gap-6">
+                    <Link href={"/"} className="flex items-center border gap-1 px-6 py-1 rounded-full border-gray-700 text-lg">Account <FaRegCircleUser /></Link>
+                    <Link href={"/"} style={{backgroundColor: Theme.primaryColor, borderColor: Theme.primaryColor}} className="px-6 py-1 rounded-full text-white border text-lg">Add Recipe</Link>
+                </div>
             </blockquote>
         </main>
     )
