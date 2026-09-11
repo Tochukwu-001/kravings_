21c4609746eb4201f4354b63ca6ed2be4b690463
Tochukwu Ozo-Osiedo <tochukwuosiedo001@gmail.com>
2026-09-11 11:14:41 +0100

    diamond

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Sansita_Swashed, Slabo_13px } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const sansitaSwashed = Sansita_Swashed({
   subsets: ["latin"],
-});
+  weight: ["300", "400", "500", "600", "700", "800", "900"]
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const slabo = Slabo_13px({
   subsets: ["latin"],
-});
+  weight: ["400"]
+})
 
 export const metadata: Metadata = {
   title: "Kravings - Africa's touch",
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${slabo.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
@@ -31,3 +31,4 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     </html>
   );
 }
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Home() {
   return (
    <main className="min-h-dvh">
     <section className="min-h-dvh bg-[url('/bg.jpg')] bg-no-repeat bg-center bg-cover">
-      <div className="min-h-dvh bg-black/60">
+      <div className="min-h-dvh bg-black/60 text-white">
         <h1>Africa's Finest Dishes</h1>
-        <p>Discover and Share your dishes </p>
+        <p>Discover and share your dishes with the world on a platform built to connect a global community of food lovers, allowing you to celebrate every unique recipe, exchange homemade flavors, and unite over a shared passion for cooking.</p>
+        <Link href={"#"}>
+          <p>See Our Dishes</p>
+          <button>
+            <FaArrowRightLong />
+          </button>
+        </Link>
       </div>
     </section>
    </main>
